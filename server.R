dad5079f623d9518c507fcc6ae313d001b568ad8
thomasyu888 <thomasyu888@gmail.com>
2015-12-03 21:13:59 -0800

    Add print

--- server.R
+++ server.R
@@ -21,6 +21,7 @@ shinyServer(function(input, output, session) {
   output$title <- renderUI({
     titlePanel(sprintf("Welcome, %s", synGetUserProfile()@userName))
     table<-synTableQuery("SELECT * FROM syn5479989 LIMIT 100")
+    print(head(table))
   })
 
 

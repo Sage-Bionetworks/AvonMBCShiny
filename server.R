5314e68db00c787af54846c7e543377729e85250
thomasyu888 <thomasyu888@gmail.com>
2016-01-06 15:21:18 -0800

    Update to data table grant selection

--- server.R
+++ server.R
@@ -11,13 +11,6 @@
 
 
 server <- function(input, output,session) {
-  #input$searchText
-  getGrantsTitles <- reactive({
-
-    
-    #table <- tableQuery()$TechAbstract
-
-  })
   
   tableQuery <- reactive({
     if (input$show_MBC) {
@@ -31,7 +24,6 @@ server <- function(input, output,session) {
         length <- unname(sapply(table$TechAbstract, function(x) {
           length(gregexpr(pattern = input$searchText,x,ignore.case = T)[[1]])
         }))
-        #print(length)
         table <- table[order(length,decreasing = T),]
       }
     } else {
@@ -40,11 +32,15 @@ server <- function(input, output,session) {
     table
   })
   
+  output$grantTitles <- DT::renderDataTable({
+    DT::datatable(tableQuery()[,c("AwardTitle","PIFirstName","PILastName","Institution")],selection = 'single')
+  },server=F)
+  
   output$mySite <- renderUI({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     author = paste(table[rowIndex, c("PILastName","PIFirstName")],collapse = ", ")
-    tags$a(href = sprintf("http://www.ncbi.nlm.nih.gov/pubmed/?term=%s+%s",author,"breast"), "NCBI resources")
+    tags$a(href = sprintf("http://www.ncbi.nlm.nih.gov/pubmed/?term=%s+%s",author,"breast"), "NCBI resources",target="_blank")
   })
   
   output$numGrants <- renderText({
@@ -61,61 +57,61 @@ server <- function(input, output,session) {
   output$PIName<-renderText({
     table <- tableQuery() 
     #rowIndex<-grep(sprintf("^%s_", input$abstractIndex), rownames(table@values))
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     paste(table[rowIndex, c("PILastName","PIFirstName")],collapse = ", ")
   })
   
   output$Institution<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Institution"]
   })
   
   output$AwardTitle<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "AwardTitle"]
   })
   
   output$Pathway<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Pathway"]
   })
   
   output$PathwayGroup<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Pathway_Group"]
   })
   
   output$MolecularTarget<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Molecular_Target"]
   })
   
   output$MolecularTargetGroup<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Molecular_Target_Group"]
   })
   
   output$MetaStage<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Metastasis_stage"]
   })
   
   output$MetaYN<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     table[rowIndex, "Metastasis_YN"]
   })
   
   output$TechAbstract<-renderText({
     table <- tableQuery() 
-    rowIndex<-grep(input$grants, table$AwardTitle)
+    rowIndex<-input$grantTitles_rows_selected
     text <- table[rowIndex, "TechAbstract"]
     
     for (word in highlight.keywords) {
@@ -129,90 +125,96 @@ server <- function(input, output,session) {
   # ---------------------------------------------
   # Dynamic Content
   # ---------------------------------------------
-  output$mutable.Pathway <- renderText({
-    table <- tableQuery() 
-    rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
-    input$button1
-    pathwayName <- isolate(input$mutable.pathway)
-   # change.annotations(rowIndex,"Pathway",pathwayName)
-    if (pathwayName != "") {
-      Dynamic.annotations@values$Pathway[rowIndex] <- pathwayName
-      synStore(Dynamic.annotations)
-      Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
-      pathwayName = ""
-    }
-    Dynamic.annotations@values$Pathway[rowIndex]
-  })
-  
-  output$mutable.PathwayGroup <- renderText({
-    table <- tableQuery() 
-    rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
-    input$button2
-    pathwaygroup <- isolate(input$mutable.pathwaygroup)
-    if (pathwaygroup != "") {
-      Dynamic.annotations@values$Pathway_Group[rowIndex] <- pathwaygroup
-      synStore(Dynamic.annotations)
-      Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
-      pathwaygroup = ""
-    }
-    Dynamic.annotations@values$Pathway_Group[rowIndex]
-  })
-  
-  output$mutable.MT <- renderText({
-    table <- tableQuery() 
-    rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
-    input$button3
-    mt <- isolate(input$mutable.mt)
-    if (mt != "") {
-      Dynamic.annotations@values$Molecular_Target[rowIndex] <- mt
-      synStore(Dynamic.annotations)
-      Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
-      mt = ""
-    }
-    Dynamic.annotations@values$Molecular_Target[rowIndex]
-  })
-  
-  output$mutable.MTGroup <- renderText({
-    table <- tableQuery() 
-    rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
-    input$button4
-    mtgroup <- isolate(input$mutable.mtgroup)
-    if (mtgroup != "") {
-      Dynamic.annotations@values$Molecular_Target_Group[rowIndex] <- mtgroup
-      synStore(Dynamic.annotations)
-      Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
-      mtgroup = ""
-    }
-    Dynamic.annotations@values$Molecular_Target_Group[rowIndex]
-  })
-  
-  output$mutable.Metayn <- renderText({
-    table <- tableQuery() 
-    rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
-    input$button5
-    metayn <- isolate(input$mutable.metayn)
-    #change.annotations()
-    if (metayn != "") {
-      Dynamic.annotations@values$Metastasis_YN[rowIndex] <- metayn
-      synStore(Dynamic.annotations)
-      Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
-      metayn = ""
-    }
-    Dynamic.annotations@values$Metastasis_YN[rowIndex]
-  })
-  
-  output$mutable.Metastage <- renderText({
-    table <- tableQuery() 
-    rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
-    input$button6
-    metastage <- isolate(input$mutable.metastage)
-    #change.annotations()
-    if (metastage != "") {
-      Dynamic.annotations@values$Metastasis_stage[rowIndex] <- metastage
-      synStore(Dynamic.annotations)
-      Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
-      metastage = ""
-    }
-    Dynamic.annotations@values$Metastasis_stage[rowIndex]
-  })
+#   output$mutable.Pathway <- renderText({
+#     table <- tableQuery() 
+#     rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
+#     input$button1
+#     pathwayName <- isolate(input$mutable.pathway)
+#    # change.annotations(rowIndex,"Pathway",pathwayName)
+#     if (pathwayName != "") {
+#       Dynamic.annotations@values$Pathway[rowIndex] <- pathwayName
+#       synStore(Dynamic.annotations)
+#       Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
+#       Dynamic.annotations@values <- Dynamic.annotations@values[!duplicated(Dynamic.annotations@values$AwardTitle),]
+#       pathwayName = ""
+#     }
+#     Dynamic.annotations@values$Pathway[rowIndex]
+#   })
+#   
+#   output$mutable.PathwayGroup <- renderText({
+#     table <- tableQuery() 
+#     rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
+#     input$button2
+#     pathwaygroup <- isolate(input$mutable.pathwaygroup)
+#     if (pathwaygroup != "") {
+#       Dynamic.annotations@values$Pathway_Group[rowIndex] <- pathwaygroup
+#       synStore(Dynamic.annotations)
+#       Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
+#       Dynamic.annotations@values <- Dynamic.annotations@values[!duplicated(Dynamic.annotations@values$AwardTitle),]
+#       pathwaygroup = ""
+#     }
+#     Dynamic.annotations@values$Pathway_Group[rowIndex]
+#   })
+#   
+#   output$mutable.MT <- renderText({
+#     table <- tableQuery() 
+#     rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
+#     input$button3
+#     mt <- isolate(input$mutable.mt)
+#     if (mt != "") {
+#       Dynamic.annotations@values$Molecular_Target[rowIndex] <- mt
+#       synStore(Dynamic.annotations)
+#       Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
+#       Dynamic.annotations@values <- Dynamic.annotations@values[!duplicated(Dynamic.annotations@values$AwardTitle),]
+#       mt = ""
+#     }
+#     Dynamic.annotations@values$Molecular_Target[rowIndex]
+#   })
+#   
+#   output$mutable.MTGroup <- renderText({
+#     table <- tableQuery() 
+#     rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
+#     input$button4
+#     mtgroup <- isolate(input$mutable.mtgroup)
+#     if (mtgroup != "") {
+#       Dynamic.annotations@values$Molecular_Target_Group[rowIndex] <- mtgroup
+#       synStore(Dynamic.annotations)
+#       Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
+#       Dynamic.annotations@values <- Dynamic.annotations@values[!duplicated(Dynamic.annotations@values$AwardTitle),]
+#       mtgroup = ""
+#     }
+#     Dynamic.annotations@values$Molecular_Target_Group[rowIndex]
+#   })
+#   
+#   output$mutable.Metayn <- renderText({
+#     table <- tableQuery() 
+#     rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
+#     input$button5
+#     metayn <- isolate(input$mutable.metayn)
+#     #change.annotations()
+#     if (metayn != "") {
+#       Dynamic.annotations@values$Metastasis_YN[rowIndex] <- metayn
+#       synStore(Dynamic.annotations)
+#       Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
+#       Dynamic.annotations@values <- Dynamic.annotations@values[!duplicated(Dynamic.annotations@values$AwardTitle),]
+#       metayn = ""
+#     }
+#     Dynamic.annotations@values$Metastasis_YN[rowIndex]
+#   })
+#   
+#   output$mutable.Metastage <- renderText({
+#     table <- tableQuery() 
+#     rowIndex<-grep(input$grants, Dynamic.annotations@values$AwardTitle)
+#     input$button6
+#     metastage <- isolate(input$mutable.metastage)
+#     #change.annotations()
+#     if (metastage != "") {
+#       Dynamic.annotations@values$Metastasis_stage[rowIndex] <- metastage
+#       synStore(Dynamic.annotations)
+#       Dynamic.annotations <-synTableQuery("SELECT * FROM syn5562008",filePath = ".")
+#       Dynamic.annotations@values <- Dynamic.annotations@values[!duplicated(Dynamic.annotations@values$AwardTitle),]
+#       metastage = ""
+#     }
+#     Dynamic.annotations@values$Metastasis_stage[rowIndex]
+#   })
 }
--- ui.R
+++ ui.R
@@ -16,113 +16,136 @@ dashboardPage(
   #       includeScript("www/readCookie.js")
   #     )
   #   ),
-  dashboardHeader(title = "Avon Metastatic Breast Cancer Abstract Analysis"),
+  dashboardHeader(title = "Avon-MBC Abstract Analysis"),
   dashboardSidebar(  
-    sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
-                                       label = "Search..."),
-    selectInput("grants", "Grants",selectize = T,
-                choices = allTitles),
-    checkboxInput('show_MBC', 'Only show MBC related grants', value = TRUE),
-    
-    conditionalPanel("input.show_MBC",
-                     selectInput("stage","Metastatic Stage",selectize = T,
-                                 choices = metaStageMenu))
-    #submitButton(text = "Apply Changes:", icon = NULL, width = NULL)
-    
+    sidebarMenu(
+      menuItem("Grant Selection", tabName = "Grant", icon = icon("dashboard")),
+      menuItem("Grant Information", tabName = "GrantInformation", icon = icon("dashboard"))
+      #submitButton(text = "Apply Changes:", icon = NULL, width = NULL)
+    )
   ),
   
   dashboardBody(
-    fluidRow(
-      box(title="Grant Info", width = 8,status = "info",
-          tags$style(type='text/css', '#AwardTitle {font-weight: bold;}'),
-          textOutput("AwardTitle"),
-          textOutput("PIName"),
-          textOutput("Institution"),
-          htmlOutput("mySite")
+    tabItems(
+      #Selection of grants
+      tabItem(tabName = "Grant",
+        fluidRow(
+          box(title="Search", width = 4,
+            p("This returns a ranked ordering of grants based on frequency of the word"),
+            sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                                    label = "Search...")
+          ),
+          box(title = "Grant Filtering", width = 4,
+              checkboxInput('show_MBC', 'Only show MBC related grants', value = TRUE),
+              conditionalPanel("input.show_MBC",
+                               selectInput("stage","Metastatic Stage",selectize = T,
+                                           choices = metaStageMenu))
+          ), 
+          box(title="Number of Grants",width = 4,status = "info",
+                 textOutput("numGrants")
+          )
+        ),
+        
+        fluidRow(
+          box(title = "Grants",width = 12,
+              DT::dataTableOutput('grantTitles')
+          )
+        )
+        
       ),
-      box(title="Number of Grants",width = 4,status = "info",
-          textOutput("numGrants")
-      )
-    ),
-    fluidRow(
-      box(title="MBC Annotations",width = 4,status = "info",
-          strong("Pathway"),
-          textOutput("Pathway"),
-          strong("Pathway Group"),
-          textOutput("PathwayGroup")
-      ),
-      box(title="MBC Annotations",width = 4,status = "info",
-          strong("Molecular Target"),
-          textOutput("MolecularTarget"),
-          strong("Molecular Target Group"),
-          textOutput("MolecularTargetGroup")
-      ),
-      box(title="MBC Annotations",width = 4,status = "info",
-          strong("Metastasis?"),
-          textOutput("MetaYN"),
-          strong("Metastasis Stage"),
-          textOutput("MetaStage")
-      )
       
-    ),
-    fluidRow(
-      box(title= "Abstract",collapsible=T, collapsed = F, width = 12,
-          tags$style(type='text/css', '#TechAbstract {font-size:10px;}'), 
-          htmlOutput("TechAbstract")
-      )
-      
-    ),
-    fluidRow(
-      box(title="Mutable Annotations",width = 4,
-          strong("Pathway"),
-          textOutput("mutable.Pathway"),
-          tags$form(
-            textInput("mutable.pathway","Input pathway here:",""),
-            actionButton("button1","Save")
+      #Grant information
+      tabItem(tabName = "GrantInformation",
+        fluidRow(
+          box(title="Grant Info", width = 8,status = "info",
+              tags$style(type='text/css', '#AwardTitle {font-weight: bold;}'),
+              textOutput("AwardTitle"),
+              textOutput("PIName"),
+              textOutput("Institution"),
+              htmlOutput("mySite")
           )
-      ),
-      box(title="Mutable Annotations",width = 4,
-          strong("Pathway Group"),
-          textOutput("mutable.PathwayGroup"),
-          tags$form(
-            textInput("mutable.pathwaygroup","Input pathway group here:",""),
-            actionButton("button2","Save")
+        ),
+        fluidRow(
+          box(title="MBC Annotations",width = 4,status = "info",
+              strong("Pathway"),
+              textOutput("Pathway"),
+              strong("Pathway Group"),
+              textOutput("PathwayGroup")
+          ),
+          box(title="MBC Annotations",width = 4,status = "info",
+              strong("Molecular Target"),
+              textOutput("MolecularTarget"),
+              strong("Molecular Target Group"),
+              textOutput("MolecularTargetGroup")
+          ),
+          box(title="MBC Annotations",width = 4,status = "info",
+              strong("Metastasis?"),
+              textOutput("MetaYN"),
+              strong("Metastasis Stage"),
+              textOutput("MetaStage")
           )
-      ),
-      box(title="Mutable Annotations",width = 4,
-          strong("Molecular Target"),
-          textOutput("mutable.MT"),
-          tags$form(
-            textInput("mutable.mt","Input molecular target here:",""),
-            actionButton("button3","Save")
+          
+        ),
+        fluidRow(
+          box(title= "Abstract",collapsible=T, collapsed = F, width = 12,
+              tags$style(type='text/css', '#TechAbstract {font-size:10px;}'), 
+              htmlOutput("TechAbstract")
           )
-      )
-    ),
-    fluidRow(
-      box(title="Mutable Annotations",width = 4,
-          strong("Molecular Target Group"),
-          textOutput("mutable.MTGroup"),
-          tags$form(
-            textInput("mutable.mtgroup","Input molecular target group here:",""),
-            actionButton("button4","Save")
-          )
-      ),
-      box(title="Mutable Annotations",width = 4,
-          strong("Metastasis"),
-          textOutput("mutable.Metayn"),
-          tags$form(
-            textInput("mutable.metayn","Input Metastasis (y/n) here:",""),
-            actionButton("button5","Save")
-          )
-      ),
-      box(title="Mutable Annotations",width = 4,
-          strong("Metastasis Stage"),
-          textOutput("mutable.Metastage"),
-          tags$form(
-            textInput("mutable.metastage","Input Metastatic stage here:",""),
-            actionButton("button6","Save")
-          )
-      )
+          
+         )#,
+#         fluidRow(
+#           box(title="Mutable Annotations",width = 4,
+#               strong("Pathway"),
+#               textOutput("mutable.Pathway"),
+#               tags$form(
+#                 textInput("mutable.pathway","Input pathway here:",""),
+#                 actionButton("button1","Save")
+#               )
+#           ),
+#           box(title="Mutable Annotations",width = 4,
+#               strong("Pathway Group"),
+#               textOutput("mutable.PathwayGroup"),
+#               tags$form(
+#                 textInput("mutable.pathwaygroup","Input pathway group here:",""),
+#                 actionButton("button2","Save")
+#               )
+#           ),
+#           box(title="Mutable Annotations",width = 4,
+#               strong("Molecular Target"),
+#               textOutput("mutable.MT"),
+#               tags$form(
+#                 textInput("mutable.mt","Input molecular target here:",""),
+#                 actionButton("button3","Save")
+#               )
+#           )
+#         ),
+#         fluidRow(
+#           box(title="Mutable Annotations",width = 4,
+#               strong("Molecular Target Group"),
+#               textOutput("mutable.MTGroup"),
+#               tags$form(
+#                 textInput("mutable.mtgroup","Input molecular target group here:",""),
+#                 actionButton("button4","Save")
+#               )
+#           ),
+#           box(title="Mutable Annotations",width = 4,
+#               strong("Metastasis"),
+#               textOutput("mutable.Metayn"),
+#               tags$form(
+#                 textInput("mutable.metayn","Input Metastasis (y/n) here:",""),
+#                 actionButton("button5","Save")
+#               )
+#           ),
+#           box(title="Mutable Annotations",width = 4,
+#               strong("Metastasis Stage"),
+#               textOutput("mutable.Metastage"),
+#               tags$form(
+#                 textInput("mutable.metastage","Input Metastatic stage here:",""),
+#                 actionButton("button6","Save")
+#               )
+#           )
+#         )
+      )#Tab item end
     )
   )
 )

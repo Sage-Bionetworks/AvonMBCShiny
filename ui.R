e118aae0c2915011e14e4db23ddddf5cccde0ff7
thomasyu888 <thomasyu888@gmail.com>
2016-03-10 21:18:50 -0800

    Re-format of platform

--- ui.R
+++ ui.R
@@ -52,141 +52,105 @@ dashboardPage(
       
       #Grant information
       tabItem(tabName = "GrantInfo",
-        column(width = 8,
-          box(title="Grant Info", width = NULL,status = "info",
-              tags$style(type='text/css', '#AwardTitle {font-weight: bold; font-size: 16px;}'),
-              textOutput("AwardTitle"),
-              textOutput("PIName"),
-              textOutput("Institution"),
-              textOutput("Date"),
-              htmlOutput("mySite"),
-              actionButton("tabBut", "View Related San Antonio Abstracts")
+        #column(width = 8,
+          fluidRow(
+            box(title="Grant Info", width = 8,status = "info",
+                tags$style(type='text/css', '#AwardTitle {font-weight: bold; font-size: 16px;}'),
+                textOutput("AwardTitle"),
+                textOutput("PIName"),
+                textOutput("Institution"),
+                textOutput("Date"),
+                htmlOutput("mySite"),
+                actionButton("tabBut", "View Related San Antonio Abstracts")
+            ),
+            box(title="MBC annotations",width = 4,collapsible=T, collapsed = F,status = "info",
+                tags$style(type='text/css', '#Pathway {font-size:10px;}'),
+                tags$style(type='text/css', '#PathwayGroup {font-size:10px;}'),
+                tags$style(type='text/css', '#MolecularTarget {font-size:10px;}'),
+                tags$style(type='text/css', '#MolecularTargetGroup {font-size:10px;}'),
+                tags$style(type='text/css', '#MetaYN {font-size:10px;}'),
+                tags$style(type='text/css', '#MetaStage {font-size:10px;}'),
+                tags$style(type='text/css', '#geneList {font-size:10px;}'),
+                strong("Pathway"),
+                textOutput("Pathway"),
+                strong("Pathway Group"),
+                textOutput("PathwayGroup"),
+                strong("Molecular Target"),
+                textOutput("MolecularTarget"),
+                strong("Molecular Target Group"),
+                textOutput("MolecularTargetGroup"),
+                strong("Metastasis?"),
+                textOutput("MetaYN"),
+                strong("Metastasis Stage"),
+                textOutput("MetaStage")
+            )
           ),
-          box(title= "Abstract",collapsible=T, collapsed = F, width = NULL,
-              tags$style(type='text/css', '#TechAbstract {font-size:12px;}'), 
-              htmlOutput("TechAbstract"),
-              bsModal("modalExample", "Data Table", "tabBut", size = "large",
-                      DT::dataTableOutput("sanantonio_abstracts")),
-              bsModal("abstractText", "Abstract Text", "rowtogg", size="large",
-                      tags$style(type='text/css', '#sanantonio_text {font-size:10px;}'),
-                      htmlOutput("sanantonio_text"))
+          fluidRow(
+            column(width=4,
+              box(title="MBC Relatedness",collapsible=T, collapsed = F,width = NULL,
+                  textOutput("mutable.Metayn"),
+                  strong("Probability that grant is MBC related:"),
+                  textOutput("MetaYNPostProb"),
+                  tags$form(
+                    selectInput("mutable.metaynmenu","Change Metastasis (y/n) here:",selectize = T,
+                                choices = c("","yes","no")),
+                    actionButton("button5","Save")
+                  ),
+                  strong("Last Updated by:"),
+                  htmlOutput("mutable.MetaYN.User")
+              ),
+              box(title="Metastatic Stage",collapsible=T, collapsed = F,width = NULL,
+                  verbatimTextOutput("mutable.Metastage"),
+                  plotOutput("MetaStagePostProb"),
+                  tags$form(
+                    selectInput("mutable.metastagemenu","Change Metastasic stage here:",selectize = T,multiple=T,
+                                choices = c("",mutablemetastage)),
+                    actionButton("button6","Save")
+                  ),
+                  strong("Last Updated by:"),
+                  htmlOutput("mutable.Metastage.User")
+              )
+            ),
+            column(width=4,
+                   box(title="Molecular Target", collapsible = T, collapsed = F, width=NULL,
+                       verbatimTextOutput("mutable.MT"),
+                       tags$form(
+                         selectInput("mutable.mtmenu","Select Molecular Target here:",selectize = T,multiple = T,
+                                     choices = c("")),
+                         actionButton("button3","Save")
+                       ),
+                       strong("Last Updated by:"),
+                       htmlOutput("mutable.MT.User")
+                   ),
+                   box(title="Pathway", collapsible = T, collapsed = F, width=NULL,
+                       #verbatimTextOutput("mutable.MT"),
+                       tags$form(
+                         selectInput("mutable.pathway","Select Pathway here:",selectize = T,multiple = T,
+                                     choices = pathways),
+                         actionButton("button4","Save")
+                       ),
+                       strong("Last Updated by:"),
+                       htmlOutput("mutable.Pathway.User")
+                   )
+            ),
+            column(width=4,
+                   box(title="Gene List",collapsible=T, collapsed = F,width = NULL,
+                       verbatimTextOutput("geneList")
+                   )
+            )
+          ),
+          fluidRow(
+            box(title= "Abstract",collapsible=T, collapsed = F, width = NULL,
+                tags$style(type='text/css', '#TechAbstract {font-size:12px;}'), 
+                htmlOutput("TechAbstract"),
+                bsModal("modalExample", "Data Table", "tabBut", size = "large",
+                        DT::dataTableOutput("sanantonio_abstracts")),
+                bsModal("abstractText", "Abstract Text", "rowtogg", size="large",
+                        tags$style(type='text/css', '#sanantonio_text {font-size:10px;}'),
+                        htmlOutput("sanantonio_text"))
+            )
           )
           
-        ),
-        column(width = 4,
-          
-          box(title="MBC annotations",width = NULL,collapsible=T, collapsed = F,status = "info",
-              tags$style(type='text/css', '#Pathway {font-size:10px;}'),
-              tags$style(type='text/css', '#PathwayGroup {font-size:10px;}'),
-              tags$style(type='text/css', '#MolecularTarget {font-size:10px;}'),
-              tags$style(type='text/css', '#MolecularTargetGroup {font-size:10px;}'),
-              tags$style(type='text/css', '#MetaYN {font-size:10px;}'),
-              tags$style(type='text/css', '#MetaStage {font-size:10px;}'),
-              tags$style(type='text/css', '#geneList {font-size:10px;}'),
-              strong("Pathway"),
-              textOutput("Pathway"),
-              strong("Pathway Group"),
-              textOutput("PathwayGroup"),
-              strong("Molecular Target"),
-              textOutput("MolecularTarget"),
-              strong("Molecular Target Group"),
-              textOutput("MolecularTargetGroup"),
-              strong("Metastasis?"),
-              textOutput("MetaYN"),
-              strong("Metastasis Stage"),
-              textOutput("MetaStage")
-          ),
-          box(title="Gene List",collapsible=T, collapsed = F,width = NULL,
-              verbatimTextOutput("geneList")
-          ),
-          box(title="MBC Relatedness",collapsible=T, collapsed = F,width = NULL,
-              textOutput("mutable.Metayn"),
-              strong("Probability that grant is MBC related:"),
-              textOutput("MetaYNPostProb"),
-              tags$form(
-                selectInput("mutable.metaynmenu","Change Metastasis (y/n) here:",selectize = T,
-                            choices = c("","yes","no")),
-                actionButton("button5","Save")
-              ),
-              strong("Last Updated by:"),
-              htmlOutput("mutable.MetaYN.User")
-          ),
-          box(title="Metastatic Stage",collapsible=T, collapsed = F,width = NULL,
-              textOutput("mutable.Metastage"),
-              plotOutput("MetaStagePostProb"),
-              tags$form(
-                selectInput("mutable.metastagemenu","Change Metastasic stage here:",selectize = T,
-                            choices = c("",metaStageMenu)),
-                actionButton("button6","Save")
-              ),
-              strong("Last Updated by:"),
-              htmlOutput("mutable.Metastage.User")
-          ),
-          box(title="Molecular Target", collapsible = T, collapsed = F, width=NULL,
-              verbatimTextOutput("mutable.MT"),
-              tags$form(
-                selectInput("mutable.mtmenu","Select Molecular Target here:",selectize = T,multiple = T,
-                            choices = c("")),
-                actionButton("button3","Save")
-              ),
-              strong("Last Updated by:"),
-              htmlOutput("mutable.MT.User")
-          )
-         
-          
-        )#,
-#         fluidRow(
-#           box(title="Mutable Annotations",width = 4,
-#               strong("Pathway"),
-#               textOutput("mutable.Pathway"),
-#               tags$form(
-#                 textInput("mutable.pathway","Input pathway here:",""),
-#                 actionButton("button1","Save")
-#               )
-#           ),
-#           box(title="Mutable Annotations",width = 4,
-#               strong("Pathway Group"),
-#               textOutput("mutable.PathwayGroup"),
-#               tags$form(
-#                 textInput("mutable.pathwaygroup","Input pathway group here:",""),
-#                 actionButton("button2","Save")
-#               )
-#           ),
-#           box(title="Mutable Annotations",width = 4,
-#               strong("Molecular Target"),
-#               textOutput("mutable.MT"),
-#               tags$form(
-#                 textInput("mutable.mt","Input molecular target here:",""),
-#                 actionButton("button3","Save")
-#               )
-#           )
-#         ),
-#         fluidRow(
-#           box(title="Mutable Annotations",width = 4,
-#               strong("Molecular Target Group"),
-#               textOutput("mutable.MTGroup"),
-#               tags$form(
-#                 textInput("mutable.mtgroup","Input molecular target group here:",""),
-#                 actionButton("button4","Save")
-#               )
-#           ),
-#           box(title="Mutable Annotations",width = 4,
-#               strong("Metastasis"),
-#               textOutput("mutable.Metayn"),
-#               tags$form(
-#                 textInput("mutable.metayn","Input Metastasis (y/n) here:",""),
-#                 actionButton("button5","Save")
-#               )
-#           ),
-#           box(title="Mutable Annotations",width = 4,
-#               strong("Metastasis Stage"),
-#               textOutput("mutable.Metastage"),
-#               tags$form(
-#                 textInput("mutable.metastage","Input Metastatic stage here:",""),
-#                 actionButton("button6","Save")
-#               )
-#           )
-#         )
       ),#Tab item end
       # Project Dashboard
       tabItem(tabName = "projDash",
